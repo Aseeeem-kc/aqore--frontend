e0bb8d7c01a7b7e9a517398a95867de2a93c826c
sagun.nepal <sagun.nepal@aqore.com>
2025-06-25 14:20:24 +0545

    Base url changed

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
- private apiUrl = 'https://localhost:7244'
+ private apiUrl = 'https://hackathonapi.aqore.com'
   constructor(private http: HttpClient) { }
 
   getAllClients(): Observable<any> {
